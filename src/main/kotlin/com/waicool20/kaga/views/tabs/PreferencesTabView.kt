46820b725245d52a088414ae5629b0fdf1e04558
waicool20 <waicool20@gmail.com>
2017-01-25 00:19:31 +0800

    Made paths in preferences tab clickable

--- src/main/kotlin/com/waicool20/kaga/views/tabs/PreferencesTabView.kt
+++ src/main/kotlin/com/waicool20/kaga/views/tabs/PreferencesTabView.kt
@@ -4,15 +4,20 @@ import com.waicool20.kaga.Kaga
 import com.waicool20.kaga.config.KagaConfig
 import javafx.fxml.FXML
 import javafx.scene.control.CheckBox
+import javafx.scene.control.Hyperlink
 import javafx.scene.control.Label
+import java.awt.Desktop
+import java.io.File
+import java.net.URI
+import java.nio.file.Path
 
 class PreferencesTabView {
     @FXML private lateinit var preventLockCheckBox: CheckBox
     @FXML private lateinit var clearConsoleCheckbox: CheckBox
     @FXML private lateinit var restartSessionCheckBox: CheckBox
     @FXML private lateinit var debugModeEnableCheckBox: CheckBox
-    @FXML private lateinit var sikulixJarPathLabel: Label
-    @FXML private lateinit var kancolleAutoRootPathLabel: Label
+    @FXML private lateinit var sikulixJarPathLink: Hyperlink
+    @FXML private lateinit var kancolleAutoRootPathLink: Hyperlink
 
     @FXML fun initialize() {
         setValues()
@@ -21,8 +26,19 @@ class PreferencesTabView {
 
     fun setValues() {
         with(Kaga.CONFIG) {
-            sikulixJarPathLabel.text = sikulixJarPath.toString()
-            kancolleAutoRootPathLabel.text = kancolleAutoRootDirPath.toString()
+            sikulixJarPathLink.setOnAction { openFile(sikulixJarPath.parent)}
+            kancolleAutoRootPathLink.setOnAction { openFile(kancolleAutoRootDirPath) }
+            sikulixJarPathLink.text = sikulixJarPath.toString()
+            kancolleAutoRootPathLink.text = kancolleAutoRootDirPath.toString()
+        }
+    }
+
+    private fun openFile(path: Path) {
+        if (Desktop.isDesktopSupported()) {
+            Thread({
+                Desktop.getDesktop().open(path.toFile())
+            }).start()
+            Kaga.ROOT_STAGE.toBack()
         }
     }
 
