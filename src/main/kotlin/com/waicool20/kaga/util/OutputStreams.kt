744c38e0fdb6ec18ab6618dca16beb67a21ed4ad
waicool20 <waicool20@gmail.com>
2017-01-29 00:08:56 +0800

    Removed cli color codes from logs

--- src/main/kotlin/com/waicool20/kaga/util/OutputStreams.kt
+++ src/main/kotlin/com/waicool20/kaga/util/OutputStreams.kt
@@ -50,6 +50,7 @@ class TextAreaOutputStream(private val console: TextArea, private val maxLines: 
 class LineListenerOutputStream : LineBufferedOutputStream() {
     override fun writeLine(line: String) {
         Kaga.LOG = if (line.contains("\u001b[2J\u001b[H")) "" else appendLineWithLimit(Kaga.LOG, line)
+                .replace("\\u001b\\[.+?m".toRegex(), "")
     }
 }
 
