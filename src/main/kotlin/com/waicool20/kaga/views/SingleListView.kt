1710cb17d1ae507e2b12f5485dd1914aa48fcbe8
waicool20 <waicool20@gmail.com>
2018-03-07 10:49:25 +0800

    Remove last item by default if nothing is selected in listview

--- src/main/kotlin/com/waicool20/kaga/views/SingleListView.kt
+++ src/main/kotlin/com/waicool20/kaga/views/SingleListView.kt
@@ -44,7 +44,7 @@ abstract class SingleListView<T>(showControlButtons: Boolean = false) : Fragment
     @FXML protected open fun onCancelButton() = closeWindow()
     @FXML protected open fun onAddButton() = Unit
     @FXML protected open fun onRemoveButton() {
-        tableView().selectedItem?.let { tableView().items.remove(it) }
+        tableView().selectedItem?.let { tableView().items.remove(it) } ?: tableView().items.remove(tableView().items.last())
     }
 
     protected open fun tableView(): TableView<T> = tableView
