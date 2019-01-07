6dc13d5e588454307428301da6c203c6fb0e3de1
waicool20 <waicool20@gmail.com>
2019-01-08 02:47:28 +0800

    Add node ZZ

--- src/main/kotlin/com/waicool20/kaga/config/KancolleAutoProfile.kt
+++ src/main/kotlin/com/waicool20/kaga/config/KancolleAutoProfile.kt
@@ -126,7 +126,7 @@ data class KancolleAutoProfile(
         const val DEFAULT_NAME = "[Current Profile]"
         val VALID_NODES = (1..12).map { it.toString() }
                 .plus("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").filter { it.isNotEmpty() })
-                .plus(listOf("Z1", "Z2", "Z3", "Z4", "Z5", "Z6", "Z7", "Z8", "Z9", "ZZ1", "ZZ2", "ZZ3"))
+                .plus(listOf("Z1", "Z2", "Z3", "Z4", "Z5", "Z6", "Z7", "Z8", "Z9", "ZZ", "ZZ1", "ZZ2", "ZZ3"))
                 .toProperty()
 
         fun load(path: Path = Kaga.CONFIG.kcaRootDirPath.resolve("config.ini")): KancolleAutoProfile {
